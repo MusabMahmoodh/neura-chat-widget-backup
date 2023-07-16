ddde7cd5f4b534f32cdc329ac0fb0165e82bc42a
musabmah <musab.18@cse.mrt.ac.lk>
2023-07-16 11:19:55 +0530

    Added heade logo

--- src/components/ConversationHeader/ConversationHeader.jsx
+++ src/components/ConversationHeader/ConversationHeader.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ResetIcon from "../../assets/reset.svg";
+import LogoImg from "../../assets/logo.svg";
 import "./ConversationHeader.scss";
 
 const ConversationHeader = ({ remoteName, resetSession }) => {
   return (
     <div className="conversation-header">
       <div className="conversation-header-data">
-        <img src="https://picsum.photos/200/300" alt={remoteName} className="conversation-header-avatar" />
+        <img src={LogoImg} alt={remoteName} className="conversation-header-avatar" />
         <div className="conversation-header-content">
           <p className="conversation-header-content-vendor-name">Esoft Chat Bot</p>
           <p className="conversation-header-content-subtitle">Hi there, let's start a conversation</p>
