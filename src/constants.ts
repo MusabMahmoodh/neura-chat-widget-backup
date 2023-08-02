5ca41af6278b84c94f7cdbf5db1d6f36d28ffe05
musabmah <musab.18@cse.mrt.ac.lk>
2023-08-02 23:22:27 +0530

    git add client support

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const CLIENT_APIS = {
+  AIEYE: "https://ai-eye-chatbot.ascii.ai",
+  ESOFT: "https://esoft-demo.ascii.ai",
+};
--- src/messageService.ts
+++ src/messageService.ts
@@ -1,5 +1,18 @@
+const getApi = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const client = urlParams.get("client");
+  if (client === "esoft") {
+    return "https://esoft-demo.ascii.ai";
+  } else if (client === "aieye") {
+    return "https://ai-eye-chatbot.ascii.ai";
+  } else {
+    return "https://esoft-demo.ascii.ai";
+  }
+};
+
 export const createSession = async (sessionId: string) => {
-  const response = await fetch(`https://esoft-demo.ascii.ai/create_session?session_id=${sessionId}`, {
+  const response = await fetch(`${getApi()}/create_session?session_id=${sessionId}`, {
     method: "GET",
     headers: {
       accept: "application/json",
@@ -9,7 +22,7 @@ export const createSession = async (sessionId: string) => {
 };
 
 export const getResponse = async (message: string, session: string) => {
-  const response = await fetch(`https://esoft-demo.ascii.ai/generate?session_id=${session}`, {
+  const response = await fetch(`${getApi()}/generate?session_id=${session}`, {
     method: "POST",
     headers: {
       accept: "application/json",
